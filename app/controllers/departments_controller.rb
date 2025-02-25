baaaf6f40e54ff8dd5f1fe815bd2bf1ae5e1b3ab
thisiscindy24 <cindyannwu@gmail.com>
2025-02-25 04:58:39 +0000

    courses

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -7,7 +7,8 @@ class DepartmentsController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
+    matching_department = Department.where({:id => the_id })
+    @department = matching_department.at(0)
 
     render({ :template => "departments/show" })
   end
